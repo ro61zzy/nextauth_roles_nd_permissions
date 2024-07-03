80ec0e4bda360e9256ba88a1a23da432a2cfba37
rozzy <rosekaremeri@gmail.com>
2024-07-03 09:59:43 +0300

    set up login options

---
+++ src/app/api/[...nextauth]/options.ts
@@ -0,0 +1,18 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {},
+      },
+    }),
+  ],
+};
